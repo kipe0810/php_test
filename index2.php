934aeb71406f243d8c6aedfe371c932be47c8f53
kipe <tkrs23280810@gmail.com>
2020-04-21 14:07:34 +0900

    php x laravel 17

--- index2.php
+++ index2.php
@@ -1,32 +1,43 @@
 <?php
 
-$height = '90';
+$height = 90;
 
-var_dump($height);
+// if ($height >= 91) {
+// 	echo '身長は91cm以上です。';
+// }
 
-if ($height === 90) {
-	echo '身長は' . $height . 'cmです。';
-}else{
-	echo '身長は90cmです。';
+// if ($height <= 90) {
+// 	echo '身長は90cm以下です。';
+// }
+
+if ($height != 90) {
+	echo '身長は90cmではありません。';
 }
 
-$signal = 'red';
+// データが入っているかどうか
+// isset empty is_null
 
-if ($signal === 'red') {
-	echo 'とまれ';
-}else if ($signal === 'yellow'){
-	echo '一旦停止';
-}else{
-	echo 'すすむ';
+$test = '1';
+
+if (!empty($test)) {
+	echo '変数は空ではありません。';
 }
 
-$speed = 80;
+// AND(&&) OR(||)
+$signal_1 = 'red';
+$signal_2 = 'blue';
 
-if ($signal === 'blue') {
-	if ($speed >= 80) {
-		echo 'スピード違反';
-	}
+if ($signal_1 === 'red' && $signal_2 === 'blue') {
+	echo '赤と青です。';
 }
 
+// 三項演算子
+// 条件 ? 真 : 偽
+
+$math = 80;
+
+$comment = $math >80 ? 'good' : 'not good';
+
+echo $comment;
 
 ?>
