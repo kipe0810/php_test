d3dcebfdc70a06222580bc38de964c37b297d6bf
kipe <tkrs23280810@gmail.com>
2020-04-22 14:22:34 +0900

    php x laravel 19

---
+++ index4.php
@@ -0,0 +1,23 @@
+<?php
+
+// for     ＜繰り返す数が決まっていたら
+// while   ＜繰り返す数が決まっていなかったら
+
+for ($i=0; $i < 10; $i++) {
+	if ($i == 5) {
+		// break;
+		continue;
+	}
+	echo $i;
+}
+
+echo '<br>';
+
+
+$j = 0;
+while ($j < 5) {
+	echo $j;
+	$j++;
+}
+
+?>
